020154ad6bcb6c995bcb07cd47cd9eeacabb0347
Dylan Turnbull <dylanturn@gmail.com>
2016-11-13 21:37:16 -0600

    more script!

--- startup-script.sh
+++ startup-script.sh
@@ -1,1 +1,6 @@
+mkdir ~/shepherdapi
+cp log4j2.xml ~/shepherdapi/
+cp shepherd-fork-stacks.xml ~/shepherdapi/
+cp udp.xml ~/shepherdapi/
+cp target/shepherd-api-1.0-jar-with-dependencies.jar ~/shepherdapi/
 java -Dlog4j.configurationFile=log4j2.xml -cp "shepherd-api-1.0-jar-with-dependencies.jar"  dylanturn.shepherdapi.testcluster.TestCluster
